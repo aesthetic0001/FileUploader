c915cf9e3df4237dca9dd18aceef4a53e889cb8e
aesthetic0001 <aesthetic0001@protonmail.com>
2023-11-02 12:19:36 -0400

    revert to unbuffered read

--- serve.go
+++ serve.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"bufio"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
 )
 
+type cachedFile struct {
+	ContentType string
+	Content     []byte
+}
+
 func handleDownloads(r *gin.Engine, saveDir string, uploaded *FileMap) {
+	fileCache := make(map[string]cachedFile)
+
+	// todo: maybe use buffer instead of reading the whole file into memory
 	r.GET("/download/:hash", func(c *gin.Context) {
 		hash := c.Param("hash")
 		file := uploaded.Files[hash]
@@ -16,12 +23,16 @@ func handleDownloads(r *gin.Engine, saveDir string, uploaded *FileMap) {
 			return
 		}
 
-		uploadedFile, _ := os.Open(saveDir + hash)
-		r := bufio.NewReader(uploadedFile)
-		content, _ := r.ReadBytes('\n')
-		uploadedFile.Close()
+		if _, exists := fileCache[hash]; !exists {
+			content, _ := os.ReadFile(saveDir + hash)
+			contentType := http.DetectContentType(content)
+			fileCache[hash] = cachedFile{
+				ContentType: contentType,
+				Content:     content,
+			}
+		}
 
-		c.Data(http.StatusOK, "application/octet-stream", content)
+		c.Data(http.StatusOK, "application/octet-stream", fileCache[hash].Content)
 	})
 
 	r.GET("/cdn/:hash", func(c *gin.Context) {
@@ -32,13 +43,16 @@ func handleDownloads(r *gin.Engine, saveDir string, uploaded *FileMap) {
 			return
 		}
 
-		uploadedFile, _ := os.Open(saveDir + hash)
-		r := bufio.NewReader(uploadedFile)
-		content, _ := r.ReadBytes('\n')
-		uploadedFile.Close()
-		contentType := http.DetectContentType(content)
+		if _, exists := fileCache[hash]; !exists {
+			content, _ := os.ReadFile(saveDir + hash)
+			contentType := http.DetectContentType(content)
+			fileCache[hash] = cachedFile{
+				ContentType: contentType,
+				Content:     content,
+			}
+		}
 
-		c.Data(http.StatusOK, contentType, content)
+		c.Data(http.StatusOK, fileCache[hash].ContentType, fileCache[hash].Content)
 	})
 
 	r.GET("/total_files", func(c *gin.Context) {
